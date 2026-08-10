b465874312eaef39c2833a9e7e486f155d279f26
ryta333 <67863324+ryta333@users.noreply.github.com>
2026-08-10 19:57:56 +0800

    study hit and  hurt and collect

--- scenes/objects/rocks/rock.gd
+++ scenes/objects/rocks/rock.gd
@@ -3,25 +3,29 @@ extends Sprite2D
 @onready var hurt_component: HurtComponent = $HurtComponent
 @onready var damage_component: DamageComponent = $DamageComponent
 
-var rock_scene = preload("res://scenes/objects/rocks/rock_drop.tscn")
+var rock_scene = preload("res://scenes/objects/rocks/stone.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print('rock ready')
 	hurt_component.hurt.connect(on_hurt)
-	damage_component.max_damaged_reached.connect(max_damaged_reached)
+	damage_component.max_damaged_reached.connect(on_max_damage_reached)
 
 func on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
+	print("hit damage")
+	material.set_shader_parameter("shake_intensity", 0.5)
+	await get_tree().create_timer(0.5).timeout
+	material.set_shader_parameter("shake_intensity", 0.0)
 	
-func max_damaged_reached() -> void:
-	call_deferred("add_rock_drop")
+func on_max_damage_reached() -> void:
+	call_deferred("add_stone_scene")
 	queue_free()
 	
-func add_rock_drop() -> void:
-	var rock_drop_instance = rock_scene.instantiate() as Node2D
-	rock_drop_instance.global_position = global_position
-	get_parent().add_child(rock_drop_instance)
+func add_stone_scene() -> void:
+	var stone_instance = rock_scene.instantiate() as Node2D
+	stone_instance.global_position = global_position
+	get_parent().add_child(stone_instance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
